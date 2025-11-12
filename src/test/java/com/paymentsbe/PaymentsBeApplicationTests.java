74d6796b1929d3ae6f4badca631db9663e2a4109
JinHyeon <tiemo0708@naver.com>
2025-11-12 17:21:07 +0900

    chore:프로젝트 기본 설정

---
+++ src/test/java/com/paymentsbe/PaymentsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
