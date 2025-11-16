3b9fe661e48b34cffba48f8d3af16a7bee3e43f9
JinHyeon <tiemo0708@naver.com>
2025-11-16 09:35:04 +0900

    feat: TimeBaseEntity 추가 및 JPA Auditing 활성화

---
+++ src/main/java/com/paymentsbe/common/entity/TimeBaseEntity.java
@@ -0,0 +1,29 @@
+package com.paymentsbe.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public abstract class TimeBaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant updatedAt;
+}
